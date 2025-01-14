f46dc4126a945595ff01611de3d409aed8c25723
antonio <tonymoya89@gmail.com>
2025-01-14 18:50:42 -0300

    cv descarga

--- public/js/script.js
+++ public/js/script.js
@@ -94,12 +94,12 @@ document.querySelectorAll('.fa-brands.fa-github').forEach(function(button) {
  // Boton que descarga El CV 
 
  document.getElementById("downloadCV").addEventListener("click", function() {
-    const pdfURL = "assets/CVSF.pdf"; 
+    const pdfURL = "public/assets/ANTONIO MOYA.pdf"; 
     window.open(pdfURL, "_blank"); // Esto abrirá el PDF en una nueva pestaña
 });
 
 document.getElementById("downloadCV").addEventListener("click", function() {
-    const pdfURL = "assets/CVSF.pdf"; 
+    const pdfURL = "public/assets/ANTONIO MOYA.pdf"; 
 
     // Preguntar al usuario si desea descargar el archivo
     if (confirm("¿Deseas descargar el archivo CV.pdf?")) {
